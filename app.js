a6c763a7194c0a188909e237a8cda7d0b9c4247a
nicolasRuarte <miqueasnruarte@protonmail.com>
2024-10-28 20:58:06 -0300

    Intentado hacer la actualizacion en tiempo real

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { guardarRegistro, obtenerRegistros, sacarTop10 } from './database.js';
+import { render } from 'ejs';
 
 const app = express();
 const PORT = 4000;
+let actualizar;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -10,7 +12,14 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
     const registros = await obtenerRegistros();
-    res.status(200).render("index.ejs", {registros: registros});
+    console.log("REGISTROS: ", registros);
+    res.status(200).render("index.ejs", {registros: registros});    
+})
+
+app.get("/obtener-registros", async (req, res) => {
+    const registros = await obtenerRegistros();
+    console.log("REGISTROS: ", registros);
+    res.status(200).render("index.ejs", {registros: registros});    
 })
 
 app.post("/enviar-datos", async (req, res) => {
