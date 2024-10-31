05c368269e0c6e25c8706e0bbc66ce25248e2985
nicolasRuarte <miqueasnruarte@protonmail.com>
2024-10-30 21:15:12 -0300

    Agregando vistas de página y el método para guardar registros

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 
-/* app.get("/", async (req, res) => {
-    const registros = await obtenerRegistros();
-    res.status(200).render("index.ejs", {registros: registros});    
+app.get("/", async (req, res) => {
+//    const registros = await obtenerRegistros();
+    res.status(200).render("index.ejs");    
 })
 
 app.get("/obtener-registros", async (req, res) => {
-    res.json( await obtenerRegistros() );
+    //res.json( await obtenerRegistros() );
 })
 
 app.get("/obtener-top10", async (req, res) => {
-    res.json(await sacarTop10());
+    //res.json(await sacarTop10());
 })
 
 app.post("/enviar-datos", async (req, res) => {
@@ -31,12 +31,8 @@ app.post("/enviar-datos", async (req, res) => {
 })
 
 app.get("/top-diez", async (req, res) => {
-    const topDiez = await sacarTop10();
-    res.status(200).render("top-diez.ejs", {topDiez: topDiez});
-}) */
-
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+    /* const topDiez = await sacarTop10(); */
+    res.status(200).render("top-diez.ejs");
+}) 
 
 app.listen(PORT);
