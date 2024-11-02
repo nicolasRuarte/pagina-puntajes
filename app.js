214ab826366479b48643f4bc6575d5e39bb55e18
nicolasRuarte <miqueasnruarte@protonmail.com>
2024-11-02 14:34:08 -0300

    Test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 app.use((req, res, next) => {
-    res.append("Access-Control-Allow-Origin", ["*"]);
+    res.append("Access-Control-Allow-Origin", ["https://pagina-puntajes.onrender.com/"]);
     res.append("Access-Control-Allow-Methods", "GET,POST");
     res.append("Access-Control-Allow-Headers", ["*"]);
     next()
