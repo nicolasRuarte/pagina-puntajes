5a2bbc2b1395563b6db4d813c8e02327910ccd81
nicolasRuarte <miqueasnruarte@protonmail.com>
2024-11-02 20:44:26 -0300

    Agregando permiso a itch.io de enviar requests

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use((req, res, next) => {
         "https://pagina-puntajes.onrender.com/",
         "https://pagina-puntajes.onrender.com/obtener-registros",
         "https://pagina-puntajes.onrender.com/obtener-top10",
-        "http://localhost:4000/"
+        "http://localhost:4000/",
+        "https://itch.io/"
     ]);
     res.append("Access-Control-Allow-Methods", "GET,POST");
     res.append("Access-Control-Allow-Headers", "Content-Type");
